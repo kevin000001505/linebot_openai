9e8e35025e3e6cef0783d82bcd98ff16106c07c7
Cheng-Hsun, Hsu <a10121555@gmail.com>
2024-09-08 16:56:17 -0400

    fix gpt version

--- app.py
+++ app.py
@@ -28,7 +28,12 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 
 def GPT_response(text):
     # 接收回應
-    response = openai.Completion.create(model="gpt-4o-mini", prompt=text, temperature=0.6, max_tokens=500)
+    response = openai.ChatCompletion.create(
+        model="gpt-4o-mini", 
+        messages=[{"role": "user", "content": text}], 
+        temperature=0.6, 
+        max_tokens=500
+    )
     print(response)
     # 重組回應
     answer = response['choices'][0]['text'].replace('。','')
