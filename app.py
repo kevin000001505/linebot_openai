8d87d79494f91a4439a99ffc66c8c19a1047f934
Cheng-Hsun, Hsu <a10121555@gmail.com>
2024-09-08 16:59:41 -0400

    keep fixcing

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def GPT_response(text):
     )
     print(response)
     # 重組回應
-    answer = response['choices'][0]['text'].replace('。','')
+    answer = response['choices'][0]['message']['content']
     return answer
 
 
