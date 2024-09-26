145abb9d8641a67baf1da210cd1a1426eb272f2b
Cheng-Hsun, Hsu <a10121555@gmail.com>
2024-09-25 22:44:04 -0400

    test version

--- app.py
+++ app.py
@@ -53,198 +53,8 @@ Preplexity_API_KEY = os.getenv('PREPLEXITY_API_KEY')
 #     "Content-Type": "application/json"
 # }
 
+# Initialize the Message_Response class
 msg_response = Message_Response()
-# Perplexity chatbot prompt
-chat = ChatPerplexity(
-    temperature=0.2,
-    model="llama-3.1-sonar-small-128k-online",
-    pplx_api_key=Preplexity_API_KEY,
-    max_tokens=2048,
-)
-
-prompt = ChatPromptTemplate.from_template("""
-You are a helpful assistant. Please respond in traditional Chinese (繁體中文).
-
-{history}
-
-User: {input}
-Assistant:
-""")
-
-conversation_with_summary = ConversationChain(
-    llm=chat,
-    prompt=prompt,
-    memory=ConversationBufferWindowMemory(k=5),
-    verbose=True,
-    )
-
-# Rephrase prompt
-rephrase_llm = ChatOpenAI(
-    openai_api_key=openai.api_key,
-    model_name="gpt-4o-mini",
-    temperature=0.8,
-    max_tokens=2048,
-)
-
-rephrase_prompt = ChatPromptTemplate.from_template("""
-根據以下對話歷史和用戶訊息，請重新表述用戶的問題，使其更清晰易懂。
-
-對話歷史：
-{history}
-
-用戶訊息：{input}
-
-重新表述後的用戶訊息：
-""")
-
-rephrase_conversation = ConversationChain(
-    llm=rephrase_llm,
-    prompt=rephrase_prompt,
-    memory=ConversationBufferWindowMemory(k=5),
-    verbose=True,
-)
-
-# Further questions prompt
-further_prompt = ChatPromptTemplate.from_template("""
-根據以下對話歷史和用戶訊息，請提供更多問題給用戶參考提問給 LLM ，讓用戶能知道還能問哪些問題。
-
-對話歷史：
-{history}
-
-用戶訊息：{input}
-
-推薦的問題：
-""")
-
-further_conversation = ConversationChain(
-    llm=rephrase_llm,
-    prompt=further_prompt,
-    memory=ConversationBufferWindowMemory(k=5),
-    verbose=True,
-)
-
-def transcribe_audio(file_path):
-    """Transcribe Audio message to text for LLM."""
-    logger.info(f"Starting transcription for file: {file_path}")
-    if not os.path.exists(file_path):
-        error_msg = f"Error: The file {file_path} does not exist."
-        logger.error(error_msg)
-        return error_msg
-
-    try:
-        with open(file_path, "rb") as audio_file:
-            transcript = openai.Audio.transcribe("whisper-1", audio_file)
-        logger.info("Transcription successful.")
-        return transcript.get("text", "無法獲取轉錄文本。")
-    except openai.error.OpenAIError as e:
-        error_msg = f"OpenAI API error during transcription: {str(e)}"
-        logger.error(error_msg)
-        return f"轉錄時發生錯誤：{str(e)}"
-    except Exception as e:
-        error_msg = f"Unexpected error during transcription: {str(e)}"
-        logger.error(error_msg)
-        return f"轉錄時發生意外錯誤：{str(e)}"
-
-def GPT_response(text):
-    """Generate GPT response."""
-    response = openai.ChatCompletion.create(
-        model="gpt-4o-mini", 
-        messages=[{"role": "user", "content": text}], 
-        temperature=0.8, 
-        max_tokens=500
-    )
-    logging.info(response)
-    # 重組回應
-    answer = response['choices'][0]['message']['content']
-    return answer
-
-def Perplexity_response(text):
-    """Perplexity response."""
-    try:
-        history = get_conversation_history(conversation_with_summary.memory)
-        rephrased_text = rephrase_user_input(text, history)
-        response = conversation_with_summary.invoke({"input": rephrased_text})
-        logger.info(f"Response: {response}")
-        further_questions = further_question(text, history)
-        return response['response'], further_questions
-    except Exception as e:
-        logger.error(f"Error running the chain: {e}")
-        return None, None
-
-def rephrase_user_input(text, history):
-    """Rephrase user message based on previous message so that LLM can better understand."""
-    try:
-        rephrase_response = rephrase_conversation.invoke({
-            "history": history,
-            "input": text
-        })
-        logger.info(f"Rephrased Input: {rephrase_response}")
-        return rephrase_response['response']
-    except Exception as e:
-        logging.error(f"重新表述時出錯: {e}")
-        return text  # 如果重新表述失敗，回傳原始輸入
-
-
-def get_conversation_history(memory):
-    """
-    從 ConversationBufferWindowMemory 中提取對話歷史，並格式化為文本。
-    
-    :param memory: ConversationBufferWindowMemory 物件
-    :return: 格式化後的對話歷史字符串
-    """
-    # 使用 load_memory_variables 來獲取 'history'
-    memory_vars = memory.load_memory_variables({})
-    return memory_vars.get('history', '')
-
-def further_question(text, history):
-    """Provide user further questions to ask."""
-    try:
-        further_questions_response = further_conversation.invoke({
-            "history": history,
-            "input": text
-        })
-        logger.info(f"Further questions: {further_questions_response}")
-        return further_questions_response['response']
-    except Exception as e:
-        logger.error(f"Error: {e}")
-        return text
-
-def encode_image(image_path):
-  # Function to encode the image
-  with open(image_path, "rb") as image_file:
-    return base64.b64encode(image_file.read()).decode('utf-8')
-    
-def Image_recognize(image_base64):
-
-    headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {openai.api_key}"
-    }
-
-    payload = {
-    "model": "gpt-4o",
-    "messages": [
-        {
-        "role": "user",
-        "content": [
-            {
-            "type": "text",
-            "text": "What’s in this image?"
-            },
-            {
-            "type": "image_url",
-            "image_url": {
-                "url": f"data:image/jpeg;base64,{image_base64}"
-            }
-            }
-        ]
-        }
-    ],
-    "max_tokens": 300
-    }
-
-    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
-    return response.json()['choices'][0]['message']['content']
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -269,8 +79,7 @@ def handle_text_message(event):
     logger.info(f"Received message: {event.message.text}")
     msg = event.message.text
     try:
-        # GPT_answer = GPT_response(msg)
-        Preplexity_answer, questions = Perplexity_response(msg)
+        Preplexity_answer, questions = msg_response.Perplexity_response(msg)
         messages = [
             TextSendMessage(text=Preplexity_answer),
             TextSendMessage(text=f"更多可參考的問題：\n{questions}")
@@ -298,12 +107,12 @@ def handle_audio_message(event):
             tempfile_path = tf.name
             logger.info(f"Audio saved to temporary file: {tempfile_path}")
         
-        msg = transcribe_audio(tempfile_path)
+        msg = msg_response.transcribe_audio(tempfile_path)
         if msg.startswith("Error"):
             raise Exception(msg)
         
         # Process the transcribed text
-        Preplexity_answer, questions = Perplexity_response(msg)
+        Preplexity_answer, questions = msg_response.Perplexity_response(msg)
         messages = [
             TextSendMessage(text=Preplexity_answer),
             TextSendMessage(text=f"更多可參考的問題：\n{questions}")
@@ -323,10 +132,9 @@ def handle_audio_message(event):
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_image_message(event):
     logger.info("Received Image message")
-    # Add your image processing logic here
     message_content = line_bot_api.get_message_content(event.message.id)
     image_base64 = base64.b64encode(message_content.content).decode('utf-8')
-    description = Image_recognize(image_base64)
+    description = msg_response.Image_recognize(image_base64)
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f'圖片描述：{description}'))
 
 @handler.add(PostbackEvent)
--- message_response.py
+++ message_response.py
@@ -6,6 +6,8 @@ from langchain_core.prompts import ChatPromptTemplate
 import os
 import logging
 import openai
+import base64
+import requests
 
 logging.basicConfig(level=logging.INFO, 
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -14,20 +16,20 @@ logger = logging.getLogger(__name__)
 class Message_Response:
 
     def __init__(self) -> None:
-        self.openai.api_key = os.getenv('OPENAI_API_KEY')
+        self.openai_api_key = os.getenv('OPENAI_API_KEY')
         self.Preplexity_API_KEY = os.getenv('PREPLEXITY_API_KEY')
-        pass
+        openai.api_key = self.openai_api_key
+        self.setup_chat_models()
 
-    def Perplexity_response(self, text):
-        """Perplexity response."""
-        chat = ChatPerplexity(
+    def setup_chat_models(self):
+        self.chat = ChatPerplexity(
             temperature=0.2,
             model="llama-3.1-sonar-small-128k-online",
             pplx_api_key=self.Preplexity_API_KEY,
             max_tokens=2048,
         )
 
-        prompt = ChatPromptTemplate.from_template("""
+        self.prompt = ChatPromptTemplate.from_template("""
         You are a helpful assistant. Please respond in traditional Chinese (繁體中文).
 
         {history}
@@ -35,16 +37,63 @@ class Message_Response:
         User: {input}
         Assistant:
         """)
-        conversation_with_summary = ConversationChain(
-        llm=chat,
-        prompt=prompt,
-        memory=ConversationBufferWindowMemory(k=5),
-        verbose=True,
+
+        self.conversation_with_summary = ConversationChain(
+            llm=self.chat,
+            prompt=self.prompt,
+            memory=ConversationBufferWindowMemory(k=5),
+            verbose=True,
         )
+
+        self.rephrase_llm = ChatOpenAI(
+            openai_api_key=self.openai_api_key,
+            model_name="gpt-4o-mini",
+            temperature=0.8,
+            max_tokens=2048,
+        )
+
+        self.rephrase_prompt = ChatPromptTemplate.from_template("""
+        根據以下對話歷史和用戶訊息，請重新表述用戶的問題，使其更清晰易懂。
+
+        對話歷史：
+        {history}
+
+        用戶訊息：{input}
+
+        重新表述後的用戶訊息：
+        """)
+
+        self.rephrase_conversation = ConversationChain(
+            llm=self.rephrase_llm,
+            prompt=self.rephrase_prompt,
+            memory=ConversationBufferWindowMemory(k=5),
+            verbose=True,
+        )
+
+        self.further_prompt = ChatPromptTemplate.from_template("""
+        根據以下對話歷史和用戶訊息，請提供更多問題給用戶參考提問給 LLM ，讓用戶能知道還能問哪些問題。
+
+        對話歷史：
+        {history}
+
+        用戶訊息：{input}
+
+        推薦的問題：
+        """)
+
+        self.further_conversation = ConversationChain(
+            llm=self.rephrase_llm,
+            prompt=self.further_prompt,
+            memory=ConversationBufferWindowMemory(k=5),
+            verbose=True,
+        )
+
+    def Perplexity_response(self, text):
+        """Perplexity response."""
         try:
-            history = self.get_conversation_history(conversation_with_summary.memory)
+            history = self.get_conversation_history(self.conversation_with_summary.memory)
             rephrased_text = self.rephrase_user_input(text, history)
-            response = conversation_with_summary.invoke({"input": rephrased_text})
+            response = self.conversation_with_summary.invoke({"input": rephrased_text})
             logger.info(f"Response: {response}")
             further_questions = self.further_question(text, history)
             return response['response'], further_questions
@@ -59,38 +108,13 @@ class Message_Response:
         :param memory: ConversationBufferWindowMemory 物件
         :return: 格式化後的對話歷史字符串
         """
-        # 使用 load_memory_variables 來獲取 'history'
         memory_vars = memory.load_memory_variables({})
         return memory_vars.get('history', '')
     
     def rephrase_user_input(self, text, history):
         """Rephrase user message based on previous message so that LLM can better understand."""
-
-        rephrase_llm = ChatOpenAI(
-            openai_api_key=openai.api_key,
-            model_name="gpt-4o-mini",
-            temperature=0.8,
-            max_tokens=2048,
-        )
-        rephrase_prompt = ChatPromptTemplate.from_template("""
-        根據以下對話歷史和用戶訊息，請重新表述用戶的問題，使其更清晰易懂。
-
-        對話歷史：
-        {history}
-
-        用戶訊息：{input}
-
-        重新表述後的用戶訊息：
-        """)
-
-        rephrase_conversation = ConversationChain(
-            llm=rephrase_llm,
-            prompt=rephrase_prompt,
-            memory=ConversationBufferWindowMemory(k=5),
-            verbose=True,
-        )
         try:
-            rephrase_response = rephrase_conversation.invoke({
+            rephrase_response = self.rephrase_conversation.invoke({
                 "history": history,
                 "input": text
             })
@@ -102,33 +126,8 @@ class Message_Response:
     
     def further_question(self, text, history):
         """Provide user further questions to ask."""
-
-        rephrase_llm = ChatOpenAI(
-            openai_api_key=openai.api_key,
-            model_name="gpt-4o-mini",
-            temperature=0.8,
-            max_tokens=2048,
-        )
-        # Further questions prompt
-        further_prompt = ChatPromptTemplate.from_template("""
-        根據以下對話歷史和用戶訊息，請提供更多問題給用戶參考提問給 LLM ，讓用戶能知道還能問哪些問題。
-
-        對話歷史：
-        {history}
-
-        用戶訊息：{input}
-
-        推薦的問題：
-        """)
-
-        further_conversation = ConversationChain(
-            llm=rephrase_llm,
-            prompt=further_prompt,
-            memory=ConversationBufferWindowMemory(k=5),
-            verbose=True,
-        )
         try:
-            further_questions_response = further_conversation.invoke({
+            further_questions_response = self.further_conversation.invoke({
                 "history": history,
                 "input": text
             })
@@ -137,3 +136,56 @@ class Message_Response:
         except Exception as e:
             logger.error(f"Error: {e}")
             return text
+
+    def transcribe_audio(self, file_path):
+        """Transcribe Audio message to text for LLM."""
+        logger.info(f"Starting transcription for file: {file_path}")
+        if not os.path.exists(file_path):
+            error_msg = f"Error: The file {file_path} does not exist."
+            logger.error(error_msg)
+            return error_msg
+
+        try:
+            with open(file_path, "rb") as audio_file:
+                transcript = openai.Audio.transcribe("whisper-1", audio_file)
+            logger.info("Transcription successful.")
+            return transcript.get("text", "無法獲取轉錄文本。")
+        except openai.error.OpenAIError as e:
+            error_msg = f"OpenAI API error during transcription: {str(e)}"
+            logger.error(error_msg)
+            return f"轉錄時發生錯誤：{str(e)}"
+        except Exception as e:
+            error_msg = f"Unexpected error during transcription: {str(e)}"
+            logger.error(error_msg)
+            return f"轉錄時發生意外錯誤：{str(e)}"
+
+    def Image_recognize(self, image_base64):
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.openai_api_key}"
+        }
+
+        payload = {
+            "model": "gpt-4o",
+            "messages": [
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": "What's in this image?"
+                        },
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/jpeg;base64,{image_base64}"
+                            }
+                        }
+                    ]
+                }
+            ],
+            "max_tokens": 300
+        }
+
+        response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+        return response.json()['choices'][0]['message']['content']
