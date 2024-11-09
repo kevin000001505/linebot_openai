4f850a5ac625720389401861af97a5f751e4dd5c
Cheng-Hsun, Hsu <a10121555@gmail.com>
2024-11-09 12:21:56 -0500

    try to fix bug

--- app.py
+++ app.py
@@ -59,9 +59,7 @@ def callback():
     signature = request.headers["X-Line-Signature"]
     # get request body as text
     body = request.get_data(as_text=True)
-    request_info = json.loads(body)
-    app.logger.info("Request Info: ", request_info['events'][0])
-    # app.logger.info("Request body: " + body)
+    app.logger.info("Request body: " + body)
     # handle webhook body
     try:
         handler.handle(body, signature)
