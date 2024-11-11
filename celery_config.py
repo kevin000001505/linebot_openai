920d1c815a1e0a7fa0dd13b95f6d79836624ef58
Cheng-Hsun, Hsu <a10121555@gmail.com>
2024-11-10 19:02:39 -0500

    try again

--- celery_config.py
+++ celery_config.py
@@ -1,20 +1,20 @@
+# celery_config.py
 from celery import Celery
 import os
 
-def make_celery(app):
+def make_celery(broker_url, backend_url):
     celery = Celery(
-        "celery_worker",
-        broker=os.environ.get('REDIS_URL'),
-        backend=os.environ.get('REDIS_URL')
+        'celery_worker',          # Name of the Celery app
+        broker=broker_url,
+        backend=backend_url
     )
-    celery.conf.update(app.config)
-    TaskBase = celery.Task
-
-    class ContextTask(TaskBase):
-        abstract = True
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-
-    celery.Task = ContextTask
+    celery.conf.update({
+        'broker_url': broker_url,
+        'result_backend': backend_url,
+        'task_serializer': 'json',
+        'result_serializer': 'json',
+        'accept_content': ['json'],
+        'timezone': 'UTC',
+        'enable_utc': True,
+    })
     return celery
